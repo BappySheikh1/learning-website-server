c9096bc68289c2689753b3ddadc86ef8771e130a
Your Name Main <you@example.com>
2022-10-25 12:59:30 -0700

    category api added

--- index.js
+++ index.js
@@ -3,9 +3,15 @@ const app =express()
 const cors=require('cors')
 const port =5000
 
+const categoryData=require('./Data/category.json')
+
 app.use(cors())
 app.get('/',(req,res)=>{
     res.send('Learning website api adde')
+})
+
+app.get('/tutorial',(req,res)=>{
+    res.send(categoryData)
 })
 
 app.listen(port ,()=>{
